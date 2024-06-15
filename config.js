b1c8226e9c58f4d228ca7a3e0d61532230880690
kashyapgaurav22 <153973339+kashyapgaurav22@users.noreply.github.com>
2024-06-15 12:19:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    titleWeb: "Gửi em",
+    titleWeb: "love you my wifey",
 
     background: "background.jpg",
 
-    min1: "min1.jfif",
-    min2: "min2.jpg",
-    min3: "max3.jpg",
-    min4: "max4.webp",
-    min5: "min5.jpg",
-    min6: "min6.jpg",
+    min1: "min1.jpeg",
+    min2: "min2.jpeg",
+    min3: "max3.jpeg",
+    min4: "max4.jpeg",
+    min5: "min5.jpeg",
+    min6: "min6.jpeg",
 
-    max1: "max1.jpg",
-    max2: "max2.jpg",
-    max3: "max3.jpg",
-    max4: "max4.webp",
-    max5: "max5.png",
-    max6: "max6.jpg",
+    max1: "max1.jpeg",
+    max2: "max2.jpeg",
+    max3: "max3.jpeg",
+    max4: "max4.jpeg",
+    max5: "max5.jpeg",
+    max6: "max6.jpeg",
 }
